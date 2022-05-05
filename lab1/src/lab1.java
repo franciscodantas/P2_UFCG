d8e40c1363a5ce9490041ac8036acdef187c4dbc
Francisco Antonio Dantas de Sousa <96065182+franciscodantas@users.noreply.github.com>
2022-05-05 19:17:28 -0300

    Create lab1.java

---
+++ lab1/src/lab1.java
@@ -0,0 +1,11 @@
+/**
+ * Laboratório de Programação 2 - Lab 1
+ * 
+ * @author Francisco Antonio Dantas de Sousa - 121110408
+ */
+
+public class lab1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Dirlididi!");
+    }   
+}
